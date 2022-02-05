b9b12971d4f97862cf8bd43b1312c024220fc1df
Shubham Sharma <shubham.sharma200121@gmail.com>
2022-02-05 11:48:57 +0530

    Morning Commit

--- app/src/main/java/com/sharma/growbusiness/SearchActivity.kt
+++ app/src/main/java/com/sharma/growbusiness/SearchActivity.kt
@@ -2,10 +2,12 @@ package com.sharma.growbusiness
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_purchase.*
 import kotlinx.android.synthetic.main.activity_search.*
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
@@ -19,27 +21,35 @@ class SearchActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
         supportActionBar?.hide()
-        SearchBtn.setOnClickListener {
-var temp=searchInput.text.toString()
-            search_recycler.layoutManager= LinearLayoutManager(this)
-            var Adapter =search_recycler.adapter
-            search_recycler.adapter=adapter
-
-            db.todoDao().get_search_sale_item().observe(this, Observer {
-
-                list.clear()
-                list.addAll(it)
-                adapter.notifyDataSetChanged()
 
 
-            })
-            GlobalScope.launch {
-                var cust_id_from_dao_function=db.todoDao().getCustomerID(temp)
-                search_customer_id.setText(""+cust_id_from_dao_function)
-                var cust_name_from_dao_function=db.todoDao().getCustomerName(temp)
-                SearchCustomerName.setText(""+cust_name_from_dao_function)
-            }
+        search_recycler.layoutManager = LinearLayoutManager(this)
+        var Adapter = search_recycler.adapter
+        search_recycler.adapter = adapter
+        db.todoDao().get_search_sale_item("100").observe(this, Observer {
 
+            list.clear()
+            list.addAll(it)
+            adapter.notifyDataSetChanged()
+
+
+        })
+        var temp=bill_no_input.text.toString()
+
+
+            SearchBtn.setOnClickListener {
+//                if(temp.length!=0) {
+
+                GlobalScope.launch (Dispatchers.IO){
+                    var cust_id_from_dao_function = db.todoDao().getCustomerID(temp)
+                    search_customer_id.setText("" + cust_id_from_dao_function)
+                    var cust_name_from_dao_function = db.todoDao().getCustomerName(temp)
+                    SearchCustomerName.setText("" + cust_name_from_dao_function)
+                }
+
+//            }else {
+//                    Toast.makeText(this, "Enter Bill No", Toast.LENGTH_SHORT).show()
+//                }
         }
     }
 }
